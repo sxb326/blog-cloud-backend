946db4ba6548af8b3446ca1057128a05adc27a2a
shixianbiao <sxb0326@qq.com>
2024-04-05 11:29:41 +0800

    网页服务端工程初始化

---
+++ blog-web-backend/src/main/java/com/xb/blog/web/BlogWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xb.blog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogWebBackendApplication.class, args);
+    }
+
+}
