51096cf8af00713516f43a20fc963a060c517aa3
shixianbiao <sxb0326@qq.com>
2024-04-05 21:13:33 +0800

    首页设计

--- blog-web-frontend/src/App.vue
+++ blog-web-frontend/src/App.vue
@@ -1,18 +1,66 @@
 <template>
-    <button @click="handleClick">click</button>
-    <h1>{{ res }}</h1>
-    <base-no-data />
-    <base-no-data>请先选择数据</base-no-data>
-  </template>
-  
-  <script setup>
-  import { getCurrentInstance } from 'vue';
-  const { proxy } = getCurrentInstance();
-  import { ref } from 'vue';
+  <el-row class="topBar">
+    <el-col :span="2">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </el-col>
+    <el-col :span="12" class="menu">
+      <div class="menuItem"><router-link to="/">首页</router-link></div>
+      <div class="menuItem"><router-link to="/test">专栏</router-link></div>
+    </el-col>
+    <el-col :span="6">
+      <el-input v-model="keyWord" style="width: 240px" placeholder="想搜索点什么呢">
+        <template #suffix>
+          <el-icon class="el-input__icon" style="cursor: pointer;" @click="doSearch">
+            <search />
+          </el-icon>
+        </template>
+      </el-input>
+    </el-col>
+    <el-col :span="1">
+      <el-icon size="25px"><Bell /></el-icon>
+    </el-col>
+    <el-col :span="1">
+      <el-icon size="25px"><User /></el-icon>
+    </el-col>
+  </el-row>
+  <router-view/>
+</template>
 
-  let res = ref(null);
-  
-  async function handleClick() {
-    res.value = await proxy.$api.test.time();
-  }
-  </script>
+<script setup>
+import { User,Search } from '@element-plus/icons-vue';
+import { ref } from 'vue';
+
+let keyWord = ref("");
+
+// 搜索
+function doSearch() {
+  alert('触发搜索,关键字:' + keyWord.value)
+}
+
+</script>
+
+<style scoped>
+.topBar {
+  border-bottom: 1px solid #afafaf;
+  margin-bottom: 10px;
+}
+
+.menu {
+  display: flex
+}
+
+.menuItem {
+  margin: 0 10px;
+  color: red;
+}
+
+.router-link-active {
+  text-decoration: none;
+  color: #646cff;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
--- blog-web-frontend/src/views/index/index.vue
+++ blog-web-frontend/src/views/index/index.vue
@@ -1,1 +1,3 @@
-<template>index</template>
+<template>
+a
+</template>
