a89ab6f2809a094643133a2e98749aae1eb40f83
shixianbiao <sxb0326@qq.com>
2024-04-19 13:18:47 +0800

    引入认证服务

---
+++ blog-auth/src/main/java/com/xb/blog/auth/BlogAuthApplication.java
@@ -0,0 +1,13 @@
+package com.xb.blog.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAuthApplication.class, args);
+    }
+
+}
