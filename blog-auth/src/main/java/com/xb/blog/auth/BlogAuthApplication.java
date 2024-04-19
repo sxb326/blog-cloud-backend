ec9c589ac68ec610279701ed70ccb65c0c42e72c
shixianbiao <sxb0326@qq.com>
2024-04-19 13:19:32 +0800

    引入认证服务

--- blog-auth/src/main/java/com/xb/blog/auth/BlogAuthApplication.java
+++ blog-auth/src/main/java/com/xb/blog/auth/BlogAuthApplication.java
@@ -2,7 +2,9 @@ package com.xb.blog.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient//开启服务的注册发现
 @SpringBootApplication
 public class BlogAuthApplication {
 
