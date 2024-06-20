acd55e3a952531840af276d10b1808f815f7c713
shixianbiao <sxb0326@qq.com>
2024-06-20 13:32:26 +0800

    优化页面样式

--- blog-vue/src/views/editor.vue
+++ blog-vue/src/views/editor.vue
@@ -1,13 +1,14 @@
 <template>
-    <el-row>
-        <el-col :span="22">
+    <el-row class="rowDiv">
+        <el-col :span="20">
             <el-input v-model="title" size="large" placeholder="请输入文章标题"></el-input>
         </el-col>
-        <el-col :span="2">
-            <el-button type="primary" style="margin: 3px 25px">发布</el-button>
+        <el-col :span="4" class="btnDiv">
+            <el-button type="warning" plain>保存草稿</el-button>
+            <el-button type="primary">发布文章</el-button>
         </el-col>
     </el-row>
-    <v-md-editor v-model="content" height="calc(100vh - 50px)"></v-md-editor>
+    <v-md-editor v-model="content" height="calc(100vh - 100px)"></v-md-editor>
 </template>
 
 <script setup>
@@ -18,5 +19,12 @@ let content = ref("");
 </script>
 
 <style scoped>
-
+.rowDiv {
+    padding: 15px;
+}
+.btnDiv{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style>
