9062cc4488630d4cfd3d7c0faeb6b71d1996020a
shixianbiao <sxb0326@qq.com>
2024-06-20 12:43:55 +0800

    使用嵌套路由优化页面格式

--- blog-vue/src/router/index.js
+++ blog-vue/src/router/index.js
@@ -1,15 +1,22 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import {createRouter, createWebHashHistory} from 'vue-router';
 
 // 本地静态路由
 export const constantRoutes = [
-  { path: '/', component: () => import('@/views/index/index.vue') },
-  { path: '/test', component: () => import('@/views/test/index.vue') },
+    {
+        path: '/',
+        component: () => import('@/views/index.vue'),
+        children: [
+            {path: '/', component: () => import('@/views/home.vue')},
+            {path: '/test', component: () => import('@/views/test/index.vue')}
+        ]
+    },
+    {path: '/edit', component: () => import("@/views/edit.vue")}
 ];
 
 // 创建路由
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes: constantRoutes,
+    history: createWebHashHistory(),
+    routes: constantRoutes,
 });
 
 export default router;
