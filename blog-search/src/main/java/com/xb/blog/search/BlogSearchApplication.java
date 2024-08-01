5287b9ac53ae1f8e52e0b3daa2df79d42bae3db6
sxb <sxb0326@qq.com>
2024-08-01 09:24:32 +0800

    检索服务

---
+++ blog-search/src/main/java/com/xb/blog/search/BlogSearchApplication.java
@@ -0,0 +1,13 @@
+package com.xb.blog.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSearchApplication.class, args);
+    }
+
+}
---
+++ blog-search/src/test/java/com/xb/blog/search/BlogSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xb.blog.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
