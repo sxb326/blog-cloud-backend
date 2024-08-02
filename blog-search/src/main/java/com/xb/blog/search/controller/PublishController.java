48ddfc437f7fdec679c5a2df40283329370e60e4
sxb <sxb0326@qq.com>
2024-08-02 15:10:58 +0800

    检索服务结构优化

---
+++ blog-search/src/main/java/com/xb/blog/search/controller/PublishController.java
@@ -0,0 +1,44 @@
+package com.xb.blog.search.controller;
+
+import cn.hutool.json.JSONUtil;
+import com.xb.blog.common.constants.Result;
+import com.xb.blog.common.pojo.BlogDocument;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.xcontent.XContentType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/publish")
+public class PublishController {
+
+    @Autowired
+    private RestHighLevelClient client;
+
+    /**
+     * 根据传入的doc对象，上传文章数据到es
+     *
+     * @param doc
+     * @return
+     */
+    @PostMapping
+    public Result publish(@RequestBody BlogDocument doc) {
+        IndexRequest indexRequest = new IndexRequest("blog_list");
+
+        indexRequest.id(doc.getUid());
+        indexRequest.source(JSONUtil.toJsonStr(doc), XContentType.JSON);
+
+        try {
+            IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+            return Result.success("文章上传到es成功！");
+        } catch (Exception e) {
+            return Result.error("文章上传到es失败！" + e.getMessage());
+        }
+    }
+}
--- blog-search/src/main/java/com/xb/blog/search/controller/SearchController.java
+++ blog-search/src/main/java/com/xb/blog/search/controller/SearchController.java
@@ -1,18 +1,16 @@
 package com.xb.blog.search.controller;
 
-import cn.hutool.json.JSONUtil;
 import com.xb.blog.common.constants.Result;
-import com.xb.blog.common.pojo.BlogDocument;
 import com.xb.blog.common.vo.SearchVo;
 import com.xb.blog.search.service.SearchService;
 import com.xb.blog.search.strategy.SearchServiceStrategy;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/search")
 public class SearchController {
 
 
@@ -25,7 +23,7 @@ public class SearchController {
      * @param keyword
      * @return
      */
-    @GetMapping("/search")
+    @GetMapping
     public Result search(String keyword, Long page) {
         //查询系统配置的检索方式 mysql、es todo 先写死 模拟系统参数
         String type = "es";
@@ -36,26 +34,5 @@ public class SearchController {
         SearchVo vo = searchService.search(keyword, page);
         searchService.beforeReturn(vo);
         return Result.success("检索成功！", vo);
-    }
-
-    /**
-     * 根据传入的doc对象，上传文章数据到es
-     *
-     * @param doc
-     * @return
-     */
-    @PostMapping("/publish")
-    public Result publish(@RequestBody BlogDocument doc) {
-        IndexRequest indexRequest = new IndexRequest("blog_list");
-
-        indexRequest.id(doc.getUid());
-        indexRequest.source(JSONUtil.toJsonStr(doc), XContentType.JSON);
-
-        try {
-//            IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-            return Result.success("文章上传到es成功！");
-        } catch (Exception e) {
-            return Result.error("文章上传到es失败！" + e.getMessage());
-        }
     }
 }
