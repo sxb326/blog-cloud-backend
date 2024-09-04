2054c14d0d582ac6f56111d2faa0f779f4c0c14c
sxb <sxb0326@qq.com>
2024-09-04 22:12:27 +0800

    关注

--- blog-web/src/main/java/com/xb/blog/web/service/impl/FollowServiceImpl.java
+++ blog-web/src/main/java/com/xb/blog/web/service/impl/FollowServiceImpl.java
@@ -5,12 +5,18 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xb.blog.common.core.utils.UserUtil;
 import com.xb.blog.web.dao.FollowDao;
 import com.xb.blog.web.entity.FollowEntity;
+import com.xb.blog.web.publisher.MessagePublisher;
 import com.xb.blog.web.service.FollowService;
 import com.xb.blog.web.vo.FollowSaveVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowDao, FollowEntity> implements FollowService {
+
+    @Autowired
+    private MessagePublisher messagePublisher;
+
     @Override
     public void save(FollowSaveVo vo) {
         Boolean isFollow = vo.getIsFollow();
@@ -19,6 +25,8 @@ public class FollowServiceImpl extends ServiceImpl<FollowDao, FollowEntity> impl
             entity.setUserUid(UserUtil.getUserId());
             entity.setTargetUserUid(vo.getTargetUserUid());
             save(entity);
+            //发送消息
+            messagePublisher.sendMessage(4, null, UserUtil.getUserId(), vo.getTargetUserUid(), null, null);
         } else {
             remove(new QueryWrapper<FollowEntity>().eq("user_uid", UserUtil.getUserId()).eq("target_user_uid", vo.getTargetUserUid()));
         }
