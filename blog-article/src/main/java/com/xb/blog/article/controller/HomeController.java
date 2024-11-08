98ba6501f2be592136b8b76a03e7090968f98528
sxb <sxb0326@qq.com>
2024-11-08 16:50:35 +0800

    首页优化

--- blog-article/src/main/java/com/xb/blog/article/controller/HomeController.java
+++ blog-article/src/main/java/com/xb/blog/article/controller/HomeController.java
@@ -1,18 +1,19 @@
 package com.xb.blog.article.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.xb.blog.common.core.constants.Result;
 import com.xb.blog.article.entity.CategoryEntity;
 import com.xb.blog.article.service.ArticleService;
 import com.xb.blog.article.service.CategoryService;
 import com.xb.blog.article.vo.ArticleListVo;
 import com.xb.blog.article.vo.ArticleTopVo;
+import com.xb.blog.common.core.constants.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -33,7 +34,13 @@ public class HomeController {
 
     @GetMapping("/category")
     public Result category() {
-        List<CategoryEntity> list = categoryService.list(new QueryWrapper<CategoryEntity>().orderByAsc("sort"));
+        List<CategoryEntity> list = new ArrayList<>();
+        CategoryEntity entity = new CategoryEntity();
+        entity.setId("");
+        entity.setName("全部");
+        entity.setIcon("HomeFilled");
+        list.add(entity);
+        list.addAll(categoryService.list(new QueryWrapper<CategoryEntity>().orderByAsc("sort")));
         return Result.success(list);
     }
 
